0ddc86f24371b78afc3e2f17faa0d36f84ff3d86
Andrew Breyen <andrew.breyen@gmail.com>
2019-10-11 16:17:42 -0500

    Added buttons to menu page for menu types

--- lib/Account.dart
+++ lib/Account.dart
@@ -7,7 +7,7 @@ class Account extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Account222'),
+        title: Text('Account'),
       ),
 
     );
--- lib/menu/MenuHome.dart
+++ lib/menu/MenuHome.dart
@@ -1,6 +1,7 @@
 // Account.dart
 
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MenuHome extends StatelessWidget {
   @override
@@ -8,6 +9,30 @@ class MenuHome extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Menu'),
+
+      ),
+      body: ListView(
+      children: <Widget>[
+
+      const SizedBox(height: 30),
+      RaisedButton(
+        onPressed: () {},
+        child: const Text(
+            'Beverages',
+            style: TextStyle(fontSize: 20)
+        ),
+      ),
+
+        const SizedBox(height: 30),
+        RaisedButton(
+          onPressed: () {},
+          child: const Text(
+              'Daily Bakery',
+              style: TextStyle(fontSize: 20)
+          ),
+        ),
+
+        ],
       ),
 
     );
