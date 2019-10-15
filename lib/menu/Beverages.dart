eab4322b0faa0bae0fdf2edb6a0bad5a5008160a
ZHEINEN001 <47953793+ZHEINEN001@users.noreply.github.com>
2019-10-14 21:07:02 -0500

    used the database controller in all the methods

--- lib/menu/Beverages.dart
+++ lib/menu/Beverages.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mcglynns_food2go/DBA.dart';
 
 class Beverages extends StatefulWidget {
--- lib/menu/GrillDaily.dart
+++ lib/menu/GrillDaily.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mcglynns_food2go/CustomCard.dart';
+import 'package:mcglynns_food2go/DBA.dart';
 
 class GrillDaily extends StatefulWidget {
   @override
@@ -17,29 +16,7 @@ class _GrillDailyState extends State<GrillDaily> {
       body: Center(
         child: Container(
             padding: const EdgeInsets.all(10.0),
-            child: StreamBuilder<QuerySnapshot>(
-              stream: Firestore.instance.collection('Grill Daily')
-                  .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError)
-                  return new Text('Error: ${snapshot.error}');
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return new Text('Loading...');
-                  default:
-                    return new ListView(
-                      children: snapshot.data.documents
-                          .map((DocumentSnapshot document) {
-                        return new CustomCard(
-                          title: document['name'],
-                          price: document['Price'],
-                        );
-                      }).toList(),
-                    );
-                }
-              },
-            )),
+            child: DBA(collection: 'Beverages')),
       ),
     );
   }
